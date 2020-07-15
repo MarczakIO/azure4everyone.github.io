cd48be9260ef491d61f7c8299d585823cd925546
Adam Marczak <adam@marczak.io>
2020-07-15 23:27:45 +0200

    new layout

--- js/main.js
+++ js/main.js
@@ -22,6 +22,29 @@
         }
         lazyLoad();
         $( window ).scroll(lazyLoad).resize(lazyLoad);
+
+        $('.check-answer').click(function(e){
+            var el = $(this);
+            var form = $(this).parent();
+            form.addClass('check-answer');
+            var answeredOk = form.find('.answer > input:checked').length;
+            var answers = form.find('.answer').length;
+            var answersNotOk = form.find(':not(.answer) > input:checked').length;
+            if (answeredOk === answers && answersNotOk === 0) {
+                form.addClass('perfect-score');
+            } else {
+                form.removeClass('perfect-score');
+            }
+        });
+        $('.reset-answer').click(function(e){
+            var el = $(this);
+            var form = $(this).parent();
+            form.removeClass('check-answer').removeClass('perfect-score');
+            $(this).siblings('div').find('input').prop('checked', false);
+        });
+        $('.input-answer').change(function(e) {
+            $(this).parent().parent().removeClass('check-answer').removeClass('perfect-score');;
+        });
     });
 
 })(window.app = window.app || {}, jQuery);
